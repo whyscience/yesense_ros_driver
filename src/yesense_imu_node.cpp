1261f72baf1bf102ae04a447a904426f97bed33e
eric <haiyang.wang@clobotics.com>
2024-06-03 22:55:03 +0800

    change for ros2

--- src/yesense_imu_node.cpp
+++ src/yesense_imu_node.cpp
@@ -1,14 +1,13 @@
-#include <geometry_msgs/Quaternion.h>
-#include <ros/ros.h>
-#include <serial/serial.h>
-#include <sensor_msgs/Imu.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <sensor_msgs/Temperature.h>
-#include <std_msgs/String.h>
-#include <std_srvs/Empty.h>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <serial_driver/serial_driver.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <sensor_msgs/msg/temperature.hpp>
+#include <std_msgs/msg/string.hpp>
 #include <string>
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_datatypes.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
 #include <fstream>
 
 #include "analysis_data.h"
@@ -20,17 +19,18 @@ enum PARSE_STATES {
     FIND_END
 };
 
-sensor_msgs::Imu g_imu;
+sensor_msgs::msg::Imu g_imu;
+rclcpp::Clock::SharedPtr clock_;
 
-void publish_imu(const ros::Publisher &imu_pub, const ros::Publisher &imu_pose_pub,
+void publish_imu(rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub,
+                 rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr imu_pose_pub,
                  const protocol_info_t &imu_data) {
     // publish imu message
-    g_imu.header.stamp = ros::Time::now();
+    g_imu.header.stamp = clock_->now();
 
-    g_imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(
-        imu_data.roll / 180.0 * M_PI, imu_data.pitch / 180.0 * M_PI,
-        imu_data.yaw / 180.0 * M_PI);
-    //   std::cout << imu_data.roll << ", " << imu_data.pitch << ", " << imu_data.yaw << ", " << std::endl;
+    tf2::Quaternion q;
+    q.setRPY(imu_data.roll / 180.0 * M_PI, imu_data.pitch / 180.0 * M_PI, imu_data.yaw / 180.0 * M_PI);
+    g_imu.orientation = tf2_ros::toMsg(q);
 
     g_imu.angular_velocity.x = imu_data.angle_x / 180.0 * M_PI;
     g_imu.angular_velocity.y = imu_data.angle_y / 180.0 * M_PI;
@@ -40,23 +40,20 @@ void publish_imu(const ros::Publisher &imu_pub, const ros::Publisher &imu_pose_p
     g_imu.linear_acceleration.y = imu_data.accel_y;
     g_imu.linear_acceleration.z = imu_data.accel_z;
 
-    imu_pub.publish(g_imu);
+    imu_pub->publish(g_imu);
 
-    geometry_msgs::PoseStamped pose;
+    geometry_msgs::msg::PoseStamped pose;
     pose.header.frame_id = "imu_link";
     pose.header.stamp = g_imu.header.stamp;
     pose.pose.position.x = 0.0;
     pose.pose.position.y = 0.0;
     pose.pose.position.z = 0.0;
-    pose.pose.orientation.w = g_imu.orientation.w;
-    pose.pose.orientation.x = g_imu.orientation.x;
-    pose.pose.orientation.y = g_imu.orientation.y;
-    pose.pose.orientation.z = g_imu.orientation.z;
-    imu_pose_pub.publish(pose);
+    pose.pose.orientation = g_imu.orientation;
+    imu_pose_pub->publish(pose);
 }
 
 int main(int argc, char **argv) {
-    serial::Serial ser;
+    drivers::serial_driver::SerialDriver ser();
     std::string port;
     int baud_rate = 460800;
     int buf_size = 1000;
@@ -72,30 +69,41 @@ int main(int argc, char **argv) {
     bool received_message = false;
     int data_packet_start;
 
-    tf::Quaternion orientation;
-    tf::Quaternion zero_orientation;
+    tf2::Quaternion orientation;
+    tf2::Quaternion zero_orientation;
 
-    ros::init(argc, argv, "yesense_imu_node");
+    rclcpp::init(argc, argv);
 
-    ros::NodeHandle private_node_handle("~");
-    private_node_handle.param<std::string>("port", port, "/dev/ttyUSB0");
-    private_node_handle.param<int>("baud_rate", baud_rate, 460800);
-    private_node_handle.param<std::string>("tf_parent_frame_id", tf_parent_frame_id, "imu_base");
-    private_node_handle.param<std::string>("tf_frame_id", tf_frame_id, "imu_link");
-    private_node_handle.param<std::string>("frame_id", frame_id, "imu_link");
-    private_node_handle.param<double>("time_offset_in_seconds", time_offset_in_seconds, 0.0);
-    private_node_handle.param<bool>("broadcast_tf", broadcast_tf, true);
-    private_node_handle.param<double>("linear_acceleration_stddev", linear_acceleration_stddev, 0.0);
-    private_node_handle.param<double>("angular_velocity_stddev", angular_velocity_stddev, 0.0);
-    private_node_handle.param<double>("orientation_stddev", orientation_stddev, 0.0);
+    auto node = rclcpp::Node::make_shared("yesense_imu_node");
+    clock_ = node->get_clock();
 
-    ros::NodeHandle nh("imu");
-    ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("data", 100);
-    ros::Publisher imu_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("pose", 100);
+    node->declare_parameter<std::string>("port", "/dev/ttyUSB0");
+    node->declare_parameter<int>("baud_rate", 460800);
+    node->declare_parameter<std::string>("tf_parent_frame_id", "imu_base");
+    node->declare_parameter<std::string>("tf_frame_id", "imu_link");
+    node->declare_parameter<std::string>("frame_id", "imu_link");
+    node->declare_parameter<double>("time_offset_in_seconds", 0.0);
+    node->declare_parameter<bool>("broadcast_tf", true);
+    node->declare_parameter<double>("linear_acceleration_stddev", 0.0);
+    node->declare_parameter<double>("angular_velocity_stddev", 0.0);
+    node->declare_parameter<double>("orientation_stddev", 0.0);
 
-    ros::Rate r(200); // 200 hz
+    node->get_parameter("port", port);
+    node->get_parameter("baud_rate", baud_rate);
+    node->get_parameter("tf_parent_frame_id", tf_parent_frame_id);
+    node->get_parameter("tf_frame_id", tf_frame_id);
+    node->get_parameter("frame_id", frame_id);
+    node->get_parameter("time_offset_in_seconds", time_offset_in_seconds);
+    node->get_parameter("broadcast_tf", broadcast_tf);
+    node->get_parameter("linear_acceleration_stddev", linear_acceleration_stddev);
+    node->get_parameter("angular_velocity_stddev", angular_velocity_stddev);
+    node->get_parameter("orientation_stddev", orientation_stddev);
 
-    // sensor_msgs::Imu g_imu;
+    auto imu_pub = node->create_publisher<sensor_msgs::msg::Imu>("imu/data", 100);
+    auto imu_pose_pub = node->create_publisher<geometry_msgs::msg::PoseStamped>("imu/pose", 100);
+
+    rclcpp::Rate r(200); // 200 hz
+
     g_imu.header.frame_id = frame_id;
 
     g_imu.linear_acceleration_covariance[0] = linear_acceleration_stddev;
@@ -110,32 +118,21 @@ int main(int argc, char **argv) {
     g_imu.orientation_covariance[4] = orientation_stddev;
     g_imu.orientation_covariance[8] = orientation_stddev;
 
-    // std::ofstream fs("/tmp/imu_data.txt");
-
     std::string input;
     std::string read;
     PARSE_STATES state = FIND_HEAD_0;
     int data_len = 0;
     int head_ind = 0;
 
-    while (ros::ok()) {
+    while (rclcpp::ok()) {
         try {
             if (ser.isOpen()) {
                 // read string from serial device
                 if (ser.available()) {
                     read = ser.read(ser.available());
-                    // char log_data[1024] = {0};
-                    // for (unsigned int i = 0; i < read.size(); i++)
-                    // {
-                    // 	char tmp[16] = {0};
-                    // 	sprintf(tmp, "%02X ", (unsigned char)read.at(i));
-                    // 	strcat(log_data, tmp);
-                    // }
-                    // fs << log_data;
-                    // fs.flush();
 
                     if (input.size() > buf_size) {
-                        ROS_WARN("input.size() > buf_size: %ld/%d", input.size(), buf_size);
+                        RCLCPP_WARN(node->get_logger(), "input.size() > buf_size: %ld/%d", input.size(), buf_size);
                         input.clear();
                         state = FIND_HEAD_0;
                     }
@@ -165,13 +162,10 @@ int main(int argc, char **argv) {
                         } else if (state == FIND_END) {
                             input += read.at(i);
                             if (input.size() >= data_len) {
-                                std::string sentence = input; //input.substr(0, data_len);
-                                // input = input.substr(data_len);
-                                // process sentense
-                                // ...
+                                std::string sentence = input;
                                 int e = analysis_data((unsigned char *) sentence.data(), sentence.size());
                                 if (e != analysis_ok) {
-                                    ROS_WARN("analysis_data() FAILED!!!! %d", e);
+                                    RCLCPP_WARN(node->get_logger(), "analysis_data() FAILED!!!! %d", e);
                                     for (unsigned int i = 0; i < sentence.size(); i++) {
                                         char aaa[1024] = {0};
                                         sprintf(aaa, "%02X ", (unsigned char) sentence.at(i));
@@ -190,28 +184,31 @@ int main(int argc, char **argv) {
             } else {
                 // try and open the serial port
                 try {
-                    ROS_INFO("port:%s, rate:%d", port.c_str(), baud_rate);
+                    RCLCPP_INFO(node->get_logger(), "port:%s, rate:%d", port.c_str(), baud_rate);
                     ser.setPort(port);
                     ser.setBaudrate(baud_rate);
                     serial::Timeout to = serial::Timeout::simpleTimeout(1000);
                     ser.setTimeout(to);
                     ser.open();
                 } catch (serial::IOException &e) {
-                    ROS_ERROR_STREAM("Unable to open serial port " << ser.getPort() << ". Trying again in 5 seconds.");
-                    ros::Duration(5).sleep();
+                    RCLCPP_ERROR(node->get_logger(), "Unable to open serial port %s. Trying again in 5 seconds.",
+                                 ser.getPort().c_str());
+                    rclcpp::sleep_for(std::chrono::seconds(5));
                 }
 
                 if (ser.isOpen()) {
-                    ROS_DEBUG_STREAM("Serial port " << ser.getPort() << " initialized and opened.");
+                    RCLCPP_DEBUG(node->get_logger(), "Serial port %s initialized and opened.", ser.getPort().c_str());
                 }
             }
         } catch (serial::IOException &e) {
-            ROS_ERROR_STREAM("Error reading from the serial port " << ser.getPort() << ". Closing connection.");
+            RCLCPP_ERROR(node->get_logger(), "Error reading from the serial port %s. Closing connection.",
+                         ser.getPort().c_str());
             ser.close();
         }
-        ros::spinOnce();
+        rclcpp::spin_some(node);
         r.sleep();
     }
 
-    // fs.close();
+    rclcpp::shutdown();
+    return 0;
 }
